682502de5bdb15bda210b10fa5471ee9a08c60a3
charleenschen <charleenc485@gmail.com>
2022-06-14 11:42:25 -0700

    Initial Commit

---
+++ seaClean2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  seaClean2
+//
+//  Created by charleen chen on 6/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
