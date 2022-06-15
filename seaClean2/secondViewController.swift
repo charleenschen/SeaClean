c96c2c6ddc69a7b413efac0764e1492660aec064
charleenschen <charleenc485@gmail.com>
2022-06-15 08:27:00 -0700

    Added second view controller to test scroll view + app icons

---
+++ seaClean2/secondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  secondViewController.swift
+//  seaClean2
+//
+//  Created by charleen chen on 6/15/22.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label.text = "THIS IS MY TEXT THIS IS MY TEXT THIS IS MY TEXT THIS IS MY TEXT THIS IS MY TEXT THIS IS MY TEXT "
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
