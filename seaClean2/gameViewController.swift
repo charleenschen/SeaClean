c93ddc39f23c21cc86966fe621f91955bac9fa1f
charleenschen <charleenc485@gmail.com>
2022-06-15 14:31:42 -0700

    Connected start button to scroll page + Added an extra game view controller

---
+++ seaClean2/gameViewController.swift
@@ -0,0 +1,31 @@
+//
+//  gameViewController.swift
+//  seaClean2
+//
+//  Created by charleen chen on 6/15/22.
+//
+
+import UIKit
+
+class gameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //btnTest.setImage(UIImage(named: "seaCleanIcon")?.withRenderingMode(.alwaysOriginal), forState: .Normal)
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var btnTest: UIButton!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
